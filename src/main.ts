474bcd567c10c85f04f0aa458f6741b071130a68
JeanCornelio <jean_cornelio17@hotmail.com>
2025-11-07 15:00:59 -0500

    Adding Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //Global prefix
   app.setGlobalPrefix('api');
+
+  //Swagger implementation
+  const config = new DocumentBuilder()
+    .setTitle('StackStep API')
+    .setDescription('StackStep Endpoints')
+    .setVersion('1.0')
+    .addTag('Goals')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3001);
 }
